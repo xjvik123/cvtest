44b8051910d57042946d4c5d24ad3f3b7e3f0693
xjvik123 <zoyashan521@gmail.com>
2024-05-12 07:25:50 +0300

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  timeout: 30000,
+  reporter: 'list',
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 15000,
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'WebKit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+};
+export default config;
